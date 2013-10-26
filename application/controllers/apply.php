6f00645942c372451afb522edbf5aa6373a7d9ad
clanrbr <clan_rbr@yahoo.com>
2013-10-26 17:04:51 +0300

    save students exams

--- application/controllers/apply.php
+++ application/controllers/apply.php
@@ -27,6 +27,36 @@ class Apply extends MY_Controller
 				$this->data['exams'][$single_line['uni_id']][$single_line['subject_id']]=$subjectinfo;
 			}
 		}
+        
+        if (!empty($_POST)) 
+        {
+            if ( !empty($_POST['data']) )
+            {
+                $data_array=json_decode($_POST['data'],true);
+                if (!empty($data_array))
+                {
+                    $insert_student_exams=array();
+                    foreach($data_array as $single_data_array)
+                    {
+                        $uni_subject_id=$this->Model_apply->get_university_subject_id($single_data_array);
+                        if ( $uni_subject_id['id']>0 )
+                        {
+                            if ($this->Model_apply->check_student_exam(1,$uni_subject_id['id']) ==0)
+                            {
+                                var_dump($uni_subject_id['id']);
+                                $insert_student_exams[]=array('student_id'=>1,'uni_subject_id'=>$uni_subject_id['id']);
+                            }
+                        }
+                    }
+
+                    if (!empty($insert_student_exams))
+                    {
+                        $this->Model_apply->save_student_exams($insert_student_exams);
+                    }
+                }
+            }
+        }
+        
 		$this->data['view'] = 'apply/index';
 		$this->load_view();
 	}
