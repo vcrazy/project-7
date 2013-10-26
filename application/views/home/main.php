cea6bc16c61f51688e8e7cf810bb82b3d5915233
Putzmitt3l <dkralev90@gmail.com>
2013-10-26 22:30:55 +0300

    Pushing some changes on head.php

--- application/views/home/main.php
+++ application/views/home/main.php
@@ -5,3 +5,24 @@
  * and open the template in the editor.
  */
 ?>
+
+	<section id="main-nav">
+		<div id="nav-container">
+			<div id="left-link-container">
+				<div id="left-link-frame">
+					<a href="#" id="exam-link" class="btn btn-default">Заявка за изпит</a>
+				</div>
+			</div>
+			<div id="right-link-container">
+				<div id="right-link-frame">
+					<a href="#" id="apply-link" class="btn btn-default">Класиране</a>
+				</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div id="bottom-bars-container">
+			<div id="left-bar" class="curtain"></div>
+			<div id="right-bar" class="curtain"></div>
+			<div class="clear"></div>
+		</div>
+	</section>
