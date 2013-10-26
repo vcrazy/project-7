b7a85b37bdfab2f5bd842b0c2c35750563e536c4
clanrbr <clan_rbr@yahoo.com>
2013-10-26 17:54:56 +0300

    added session

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,29 +4,29 @@ class Home extends MY_Controller
 {
         protected $data;
 
-                public function __construct()
-                {
-                        parent::__construct();
+        public function __construct()
+        {
+                parent::__construct();
 
-                        $this->load->model('Model_home');//???
-                }
+                $this->load->model('Model_home');//???
+        }
         
         public function index() 
         {
-                $this->data['content_title'] = 'Начална страница';
-                $this->data['view'] = 'home/index';
-                $this->getIndexData();
-		$this->load_view();
-
+            $this->data['content_title'] = 'Начална страница';
+            $this->data['view'] = 'home/index';
+            $this->getIndexData();
+            $this->load_view();
         }
         
         public function getIndexData(){
-            if (!empty($_POST)) {
-                $this->load->helper(array('url'));
-                $this->session->set_userdata(array(
-                    //Ado da mi napravi funkciq koqto vrushta masiv
-                    //dobata forma
-                ));
+            if (!empty($_POST)) 
+            {
+                $user_data=$this->Model_home->load_user_info($user_id=1);
+                if(!empty($user_data))
+                {
+                    $this->session->set_userdata(array($user_data));
+                }
                 //redirect to profile.....
                 header("Location:/apply");
                 exit();
