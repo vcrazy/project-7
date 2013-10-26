6b4249424b5e2d11c6a4902a76a19f25621370a4
Putzmitt3l <dkralev90@gmail.com>
2013-10-26 18:49:54 +0300

    Adding post method to login form markup.

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -5,7 +5,7 @@
 			<div id="right-curtain" class="curtain"></div>
 			<div class="clear"></div>
 		</div>
-		<form id="login" role="form">
+		<form id="login" role="form" method="post">
 			<input id="name-input" class="form-control" type="text" placeholder="UserName" ><br>
 			<input id="pass-input" class="form-control" type="text" placeholder="Pass"> <br>
 			<button id="login-submit" type="submit" class="btn btn-default">log in</button>
