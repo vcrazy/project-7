be7ce4fdf9ecce84a7df9dea9470f1538bccc7f5
rosislavm <rosislavm@abv.bg>
2013-10-26 12:05:50 +0300

    test

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,4 +8,37 @@ class Home extends MY_Controller
 
 		$this->load_view();
 	}
+        
+        public function profileData()
+        {
+            // Initialize variables and set to empty strings
+             $email="";
+             $password="";
+
+            // Validate input and sanitize
+if ($_SERVER['REQUEST_METHOD']== "POST") {
+   if (empty($_POST["firstName"])) {
+      $firstNameErr = "First name is required";
+   }
+   else {
+      $firstName = test_input($_POST["firstName"]);
+   }
+   if (empty($_POST["lastName"])) {
+      $lastNameErr = "Last name is required";
+   }
+   else {
+      $lastName = test_input($_POST["lastName"]);
+   }
 }
+
+// Sanitize data
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+        }
+}
+
+
