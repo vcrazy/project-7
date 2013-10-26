1217f218aeb4654e74d47c74a9a3731ad3b1bc63
vcrazy@abv.bg <vcrazy@abv.bg>
2013-10-26 21:57:16 +0300

    login link fix

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -6,8 +6,8 @@
 			<div class="clear"></div>
 		</div>
 		<form id="login" role="form" method="post" action="">
-			<input id="name-input" class="form-control" type="text" placeholder="Потребителско име" /><br >
-			<input id="pass-input" class="form-control" type="text" placeholder="Парола" /> <br />
+			<input id="name-input" class="form-control" type="text" placeholder="Потребителско име" name="user" /><br >
+			<input id="pass-input" class="form-control" type="text" placeholder="Парола" name="pass" /> <br />
 			<button id="login-submit" type="submit" class="btn btn-default">Вход</button>
 		</form>
 	</section>
