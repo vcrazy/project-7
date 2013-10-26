8bed4869b8d7b8ae4d027915ec1518267446df40
clanrbr <clan_rbr@yahoo.com>
2013-10-26 10:34:32 +0300

    bootstrap added

---
+++ application/logs/log-2013-10-26.php
@@ -0,0 +1,46 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-10-26 08:53:37 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 08:53:37 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 08:53:37 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 08:53:42 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 08:53:42 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 08:53:42 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 08:53:45 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 08:53:45 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 08:53:45 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:26:22 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:26:22 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:26:22 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:26:24 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:26:24 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:26:24 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:26:42 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:26:42 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:26:42 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:26:43 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:26:43 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:26:43 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:27:07 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:27:07 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:27:07 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:27:08 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:27:08 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:27:08 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:27:20 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:27:20 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:27:20 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:27:21 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:27:21 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:27:21 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:27:42 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:27:42 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:27:42 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:27:43 --> Severity: Notice  --> Undefined index: project7_db C:\xampp\htdocs\project-7\application\config\database.php 52
+ERROR - 2013-10-26 09:27:43 --> Severity: Notice  --> Undefined index: project7_user C:\xampp\htdocs\project-7\application\config\database.php 53
+ERROR - 2013-10-26 09:27:43 --> Severity: Notice  --> Undefined index: project7_pass C:\xampp\htdocs\project-7\application\config\database.php 54
+ERROR - 2013-10-26 09:30:16 --> Unable to select database: t3stovet_project7
+ERROR - 2013-10-26 09:30:16 --> Unable to select database: t3stovet_project7
+ERROR - 2013-10-26 09:30:17 --> Unable to select database: t3stovet_project7
+ERROR - 2013-10-26 09:30:30 --> Unable to select database: t3stovet_project7
+ERROR - 2013-10-26 09:30:31 --> Unable to select database: t3stovet_project7
--- application/views/common/head.php
+++ application/views/common/head.php
@@ -2,6 +2,10 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<title>Project 6</title>
+		<title>Project 7</title>
+                <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+                <link href="/css/styles.css" rel="stylesheet" type="text/css" />
+                <script src="/js/jquery.min.js" type="text/javascript"></script>
+		<script src="/js/bootstrap.min.js" type="text/javascript"></script>
 	</head>
 	<body>
