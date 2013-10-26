399e97089a7283e154dcfc6238c86b5ee7ca7ce4
clanrbr <clan_rbr@yahoo.com>
2013-10-26 13:53:38 +0300

    added some functions

--- application/controllers/apply.php
+++ application/controllers/apply.php
@@ -6,7 +6,10 @@ class Apply extends MY_Controller
 	{
 //          array {
 //            'unis'->array('id'=>'SU','5'=>'..'),
-//            'exams'->array('id_unis'=>array('izpit_id'=>array('name'=>'name','info'=>'...') ))
+//            'exams'->array(
+//                          'id_unis'=>array(
+//                                              'izpit_id'=>array(
+//                                                                 'name'=>'name','info'=>'...') ))
 //            }
 //            apply
 //            
@@ -14,7 +17,28 @@ class Apply extends MY_Controller
 		$this->_set_dummy_data();
 
                 $this->load->model('Model_apply');
-                $this->Model_apply->get_universities();
+                $universities_subjects=$this->Model_apply->get_universities_subject();
+                
+                $this->data['unis'] = array();
+                $this->data['exams']=array();
+                if (!empty($universities_subjects)) 
+                {
+                    foreach ($universities_subjects as $single_line) 
+                    {
+                        $this->data['unis'][$single_line['uni_id']]=$single_line['uni_name'];
+                        $subjectinfo=array('name'=>$single_line['subject_name'],'info'=>'alabla data '.$single_line['dateof']);
+                        if (!isset($this->data['exams'][$single_line['uni_id']]))
+                        {
+                            $this->data['exams'][$single_line['uni_id']]=array();
+                        }
+                        if (!isset($this->data['exams'][$single_line['uni_id']][$single_line['subject_id']]))
+                        {
+                           $this->data['exams'][$single_line['uni_id']][$single_line['subject_id']]=array();
+                        }
+
+                        $this->data['exams'][$single_line['uni_id']][$single_line['subject_id']]=$subjectinfo;
+                    }
+                }
 		$this->data['view'] = 'apply/index';
 		$this->load_view();
 	}
@@ -22,44 +46,44 @@ class Apply extends MY_Controller
 	private function _set_dummy_data()
 	{
 		// dummy data
-		$this->data['unis'] = array(
-			1 => 'Sofia University',
-			2 => 'Other University',
-			10 => 'Another University'
-		);
+//		$this->data['unis'] = array(
+//			1 => 'Sofia University',
+//			2 => 'Other University',
+//			10 => 'Another University'
+//		);
 
-		$this->data['exams'] = array(
-			1 => array(
-				50 => array(
-					'name' => 'Math 1',
-					'info' => 'Info 1'
-				),
-				51 => array(
-					'name' => 'Math 2',
-					'info' => 'Info 2'
-				),
-				101 => array(
-					'name' => 'Bg lang',
-					'info' => 'Bg info'
-				)
-			),
-			2 => array(
-				52 => array(
-					'name' => 'Math 3',
-					'info' => 'Info 3'
-				),
-				53 => array(
-					'name' => 'Math 4',
-					'info' => 'Info 4'
-				)
-			),
-			10 => array(
-				54 => array(
-					'name' => 'Math 3',
-					'info' => 'Info 3'
-				)
-			)
-		);
+//		$this->data['exams'] = array(
+//			uni_id => array(
+//				subject_id => array(
+//					'name' => 'Math 1',
+//					'info' => 'Info 1',
+//				),
+//				51 => array(
+//					'name' => 'Math 2',
+//					'info' => 'Info 2'
+//				),
+//				101 => array(
+//					'name' => 'Bg lang',
+//					'info' => 'Bg info'
+//				)
+//			),
+//			2 => array(
+//				52 => array(
+//					'name' => 'Math 3',
+//					'info' => 'Info 3'
+//				),
+//				53 => array(
+//					'name' => 'Math 4',
+//					'info' => 'Info 4'
+//				)
+//			),
+//			10 => array(
+//				54 => array(
+//					'name' => 'Math 3',
+//					'info' => 'Info 3'
+//				)
+//			)
+//		);
 		// end of dummy data
 	}
 }
--- application/models/model_apply.php
+++ application/models/model_apply.php
@@ -4,8 +4,20 @@ class Model_apply extends MY_Model
 {	
 	public function get_universities()
 	{
-            
+            $this->db->select('id,name');
+            $this->db->from('universities');
+            $query = $this->db->get();
+            return $this->to_hash_results($query,'id');
 	}
+        
+        public function get_universities_subject() {
+            $this->db->select('university_subject.id,university_subject.uni_id,universities.name as uni_name,university_subject.subject_id,subjects.name as subject_name,university_subject.dateof');
+            $this->db->from('university_subject');
+            $this->db->join('subjects', 'university_subject.subject_id=subjects.id');
+            $this->db->join('universities', 'universities.id = university_subject.uni_id', 'left');
+            $query = $this->db->get();
+            return $this->results($query);
+        }
 //        public function get($sp_id)
 //	{
 //		$this->db->select('*');
