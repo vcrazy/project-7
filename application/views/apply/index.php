670e5b14e7e9336dce090e3b18b3fba73380fa93
clanrbr <clan_rbr@yahoo.com>
2013-10-27 13:46:42 +0200

    apply styling

--- application/views/apply/index.php
+++ application/views/apply/index.php
@@ -5,28 +5,30 @@
 	});
 </script>
 
-<form method="post" action="">
-	<select id="uni">
-		<option value="0">Моля, изберете университет</option>
-
-	<?php foreach($unis as $uni_id => $uni_name): ?>
-
-		<option value="<?php echo $uni_id; ?>"><?php echo $uni_name; ?></option>
-
-	<?php endforeach; ?>
-
-	</select>
-
-	<?php foreach($exams as $uni_id => $uni_exams): ?>
-
-		<select id="exams_<?php echo $uni_id; ?>" class="hidden uni_change">
-				<option value="0">Моля, изберете изпит</option>
-			<?php foreach($uni_exams as $exam_id => $exam_data): ?>
-				<option value="<?php echo $exam_id; ?>"><?php echo $exam_data['name']; ?></option>
-			<?php endforeach; ?>
-		</select>
-
-	<?php endforeach; ?>
+<div class="form_container">
+    <form method="post" action="">
+        <h1>ЖЕЛАЯ ДА СЕ ЯВЯ НА ИЗПИТ В:</h1>
+        <span class="form_span_info">Университет:</span>  <select id="uni">
+            <option value="0">Моля, изберете университет</option>
+            <?php foreach($unis as $uni_id => $uni_name): ?>
+                <option value="<?php echo $uni_id; ?>"><?php echo $uni_name; ?></option>
+            <?php endforeach; ?>
+        </select>
+        
+        <!--class="hidden" -->
+        <div id="specialty_show" >
+            <h1>НА СЛЕДНИТЕ ИЗПИТИ:</h1>
+            <?php foreach($exams as $uni_id => $uni_exams): ?>
+            <select id="exams_<?php echo $uni_id; ?>" class="hidden uni_change">
+                    <option value="0">Моля, изберете изпит</option>
+                <?php foreach($uni_exams as $exam_id => $exam_data): ?>
+                    <option value="<?php echo $exam_id; ?>"><?php echo $exam_data['name']; ?></option>
+                <?php endforeach; ?>
+            </select> <span id="exam_info"> Дата: 25.05.2013</span>
+            <?php endforeach; ?>
+        </div>
+    </form>
+</div>
 
 	<input type="button" id="save" value="Избери" class="hidden" />
 
@@ -39,5 +41,3 @@
 	</div>
 
 	<input type="submit" id="send" value="Изпрати всички" class="hidden" />
-
-</form>
