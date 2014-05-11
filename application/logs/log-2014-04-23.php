312149e80ddcd990898a1d07deb21442c3dc85c1
Vihren Ganev <vcrazy@abv.bg>
2014-05-11 15:38:29 +0300

    css

---
+++ application/logs/log-2014-04-23.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-04-23 21:34:57 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\project-7\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2014-04-23 21:35:00 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\project-7\system\database\drivers\mysql\mysql_driver.php 91
